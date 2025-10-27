527f5ffca829dd686938a53a3c2c972cb417e818
ematevez <ematevez@gmail.com>
2025-10-27 16:23:01 -0300

    back fix

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // await mongoose.connect("mongodb+srv://user:user@cluster0.5cff1qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-    // await mongoose.connect("mongodb+srv://anibalcaeiro:WKAVCJwJheqziPgN@cluster0.g4hk2z4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    // await mongoose.connect("mongodb+srv://user:user@cluster0.zirxpbn.mongodb.net/?appName=Cluster0");
     await mongoose.connect("mongodb+srv://user:user@cluster0.zirxpbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     
